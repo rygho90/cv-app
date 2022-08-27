f5d00a932fbfdd2b7a391c3c4543e71b59e381c0
Ryan <rygho90@gmail.com>
2022-08-27 12:11:10 -0400

    Add basic styling for main container and side bar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div className="main-container">
+        <div className="side-bar">
+          <div className="side-tab active-tab">General Information</div>
+          <div className="side-tab">Education</div>
+          <div className="side-tab">Work Experience</div>
+          <div className="side-tab">Skills</div>
+          <div className="side-tab">Review and Submit</div>
+        </div>
+        <div className="content-screen">
+          <h1>General Information</h1>
+        </div>
+      </div>
+    )
   }
 }
 
