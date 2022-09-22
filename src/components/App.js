6fad4314c8daaaed723e37d58154ae99cb96b583
Ryan <rygho90@gmail.com>
2022-09-22 11:59:43 -0400

    Set up very basic CSS/JSX with dummy text

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from "react";
+import "../css/app.css";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <div className="main-container">
+      <div className="cv">
+        <header>
+          <div className="heading">
+            <h1>John Smith</h1>
+            <h2>Worker</h2>
+          </div>
+          <div className="details-group">
+            <div>Phone: 555-5555</div>
+            <div>Email: totallyrealemail@mail.com</div>
+            <div>Location: Anytown, U.S.A</div>
+          </div>
+        </header>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae
+          pellentesque neque. Nunc convallis arcu sed dictum feugiat. Mauris
+          pretium scelerisque est non semper. Morbi feugiat lacus et massa
+          feugiat sollicitudin. 
+        </p>
+        <div>
+          <h2>Work Experience</h2>
+          <p>This is my work experience.</p>
+        </div>
+        <div>
+          <h2>Education</h2>
+          <p>This is my edumucation.</p>
+        </div>
+        <div>
+          <h2>Skills</h2>
+          <p>They pay the bills.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
